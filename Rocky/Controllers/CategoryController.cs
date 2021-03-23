5ab816744970f9ec825f45b9f3839bf065696b40
Felipe <felipe.pereira.dev@gmail.com>
2021-03-23 16:07:22 -0300

    feat: get and display all categories

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,17 @@ namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objList = _db.Categories;
+            return View(objList);
         }
     }
 }
